a6306415b8669796b051fa7572ef8b3ef151e386
Student_6 <codecat.ua@gmail.com>
2026-05-09 17:10:54 +0300

    2 commit

--- src/main/java/com/example/calendarimages/HelloApplication.java
+++ src/main/java/com/example/calendarimages/HelloApplication.java
@@ -11,9 +11,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setMinWidth(500);
+        stage.setMinHeight(350);
+        stage.setMaxWidth(1000);
+        stage.setMaxHeight(800);
         stage.show();
     }
 
